21f8b8a4eb9f73679689e7135442af0dcba2ccd6
Bodhvi Gaur <bodhvigaur@gmail.com>
2024-06-19 23:14:38 -0400

    Time: 2 ms (20.92%), Space: 45.8 MB (5.68%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -4,38 +4,73 @@ class Solution {
     zero in them and store it in a flag array.
     2. Then in seconds run, we will set all rows and columns to 0 where rowFlag[i]=1
     or colFlag[i]=1
-    3. Issue : Space : O(Max(m,n)). To improve on 
+    3. Issue : Space : O(Max(m,n)). To improve on and keep space O(1), 
+    we will use the 0th element of matrix as flag.  
+     * We will iterate from 1st index to m/n instead of 0th in order to avoid setting
+     entire array to 0
     
     */
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int rowFlag[] = new int[m];
-        int colFlag[] = new int[n];
+        boolean col0SetToZero = false;
+        boolean row0SetToZero = false;
+        boolean wasAlreadyZero = false;
 
         for (int i = 0; i<m; i++) {
             for (int j = 0; j<n ; j++) {
                 if (matrix[i][j] == 0) {
-                    rowFlag[i] = 1;
-                    colFlag[j] = 1;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                    if (j == 0 && i == 0) {
+                        wasAlreadyZero = true;
+                    }
+                    if (j == 0) {
+                        col0SetToZero = true;
+                    }
+                    if (i == 0) {
+                        row0SetToZero = true;
+                    }
                 }
+                
             }
         }
 
-        for (int i = 0; i<m; i++) {
-            if (rowFlag[i] == 1) {
+        for (int i = 1; i<m; i++) {
+            if (matrix[i][0] == 0) {
                 for (int j = 0; j<n ; j++) {
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        for (int j = 0; j<n ; j++) {
-            if (colFlag[j] == 1) {
+        for (int j = 1; j<n ; j++) {
+            if (matrix[0][j] == 0) {
                 for (int i = 0; i<m; i++) {
                     matrix[i][j] = 0;
                 }
             }
         }
+
+        if (wasAlreadyZero == true) {
+            for (int i = 1; i<m ; i++) {
+                matrix[i][0] = 0;
+            }
+            for (int j = 1; j<n ; j++) {
+                matrix[0][j] = 0;
+            }
+        } 
+        if (col0SetToZero) {
+             for (int i = 1; i<m ; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        if (row0SetToZero) {
+           for (int j = 1; j<n ; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+
     }
 }
