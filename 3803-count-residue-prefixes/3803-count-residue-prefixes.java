37a7369c1761fd75eff5f08aec0a2f4e93a05c06
Bodhvi Gaur <bodhvigaur@gmail.com>
2026-01-19 10:07:39 -0800

    Time: 3 ms (15.03%), Space: 44.1 MB (56.44%) - LeetHub

--- 3803-count-residue-prefixes/3803-count-residue-prefixes.java
+++ 3803-count-residue-prefixes/3803-count-residue-prefixes.java
@@ -7,6 +7,13 @@ class Solution {
         (0 to index), check number(unique char) = len(prefix)%3.
         3. Return count.
         4. Edge case : length = 0, object null, return 0;
+
+
+        Approach 2 : Better because space can be O(1) - space of hashset will be 3. 
+        Here, as soon as length of hashset is > 2, we can break, 
+        simply because the value of x%3 can only be 0,1,2. So 
+        no other equality will help contribute to the count.
+        So we add the condition that if size of set > 2, we break and return answer.
         **/
 
         HashSet<Character> set = new HashSet();
@@ -19,14 +26,13 @@ class Solution {
 
         int size = s.length();
         for(int i = 0; i < size; i++) {
-            char newChar = s.charAt(i);
-            if (!set.contains(newChar)) {
-                set.add(newChar);
-            }
-
-            prefix = prefix.append(newChar);
+            set.add(s.charAt(i));
+            prefix = prefix.append(s.charAt(i));
             if (prefix.length()%3 == set.size()) {
                 count++;
+            }
+            if (set.size() > 2) {
+                break;
             }
         }
         return count;
